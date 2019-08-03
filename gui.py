0735a1e1cb24a34b289f5ea634879ce3dfeb222e
Jessimoso <45774356+Jessimoso@users.noreply.github.com>
2019-08-02 21:17:49 -0700

    Adding header template to gui file

---
+++ gui.py
@@ -0,0 +1,13 @@
+
+'''
+    Filename:GUI
+    Date:02 AUG 2019
+    Purpose:GUI for Strangle Hold
+
+    -----------------------------------------------------
+    Version     Date        Author      Description
+    -----------------------------------------------------
+    <1.0>      02 AUG 2019  J.EITING
+'''
+
+
